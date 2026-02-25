e403d412a9663dc6575289ebf9b30bcfa37cd1f6
hyoseon <wink2090@naver.com>
2026-02-25 23:32:12 +0900

    feat: levelup data asset create

---
+++ D1/Source/D1/AbilitySystem/Data/D1LevelupInfo.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Data/D1LevelupInfo.h"
+
+int32 UD1LevelupInfo::FindLevelForXP(int32 XP)
+{
+	int32 Level = 1;
+	bool bSearching = true;
+	while (bSearching)
+	{
+		// LevelUpInformation[1] = Level 1 Information
+		// LevelUpInformation[2] = Level 1 Information
+		if (LevelupInformation.Num() - 1 <= Level) return Level;
+
+		if (XP >= LevelupInformation[Level].LevelupRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+	return Level;
+}
---
+++ D1/Source/D1/AbilitySystem/Data/D1LevelupInfo.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "D1LevelupInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FD1LevelupTagInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 LevelupRequirement = 0;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 AttributePointAward = 1;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 SpellPointAward = 1;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class D1_API UD1LevelupInfo : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly)
+	TArray<FD1LevelupTagInfo> LevelupInformation;
+
+	int32 FindLevelForXP(int32 XP);
+};
